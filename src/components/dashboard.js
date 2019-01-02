1cda8bcaadf178bb78999fbab2abd88d4a6db837
michael lennerblom <mlennerblom@gmail.comgit config --listgit config --global user.name Michael Lennerblomgit config --global user.email mlennerblom@gmail.comgit config --listgit config --global user.name Michael>
2019-01-01 20:43:07 -0800

    connected form to store

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,12 +1,25 @@
 import React, { Component, Fragment } from 'react';
+import { connect } from 'react-redux';
 import ChoreForm from './choreForm';
-export default class Dashboard extends Component {
+
+import {choreAdd} from '../lib/choresReducer';
+class Dashboard extends Component {
     render() {
         return (
             <Fragment>
               <h1>add dashboard page</h1>
-              <ChoreForm/>
+              <ChoreForm onComplete={this.props.choreAdd}/>
             </Fragment>
         );
     }
 }
+
+const mapStateToProps = (state) => ({ 
+    chore: state.chore,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+    choreAdd: chore => dispatch(choreAdd(chore)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
--- src/lib/choresReducer.js
+++ src/lib/choresReducer.js
@@ -2,14 +2,14 @@ import superagent from 'superagent';
 import uuid from 'uuid/v4';
 const url = 'http://localhost:3019/api/v1/user';
 // Actions
-export const CHORES_ADD = 'chores/ADD';
+export const CHORE_ADD = 'chore/ADD';
 
 // Reducer
 let initialState = [];
 export default function choreReducer(state = initialState, action) {
     let {type,payload} = action;
     switch (type) {
-        case CHORES_ADD:
+        case CHORE_ADD:
         localStorage.setItem('state', 'state');
             return [...state, payload];
         default: return state;
@@ -20,13 +20,13 @@ export default function choreReducer(state = initialState, action) {
 // export function ActionCreator() {
     
 // }
-export const choresAdd = (chores) => {
-    console.log('addReducer', chores);
-    chores.id = uuid();
+export const choreAdd = (chore) => {
+    console.log('addReducer', chore);
+    chore.id = uuid();
     return dispatch => {
-      superagent.post(url, chores)
+      superagent.post(url, chore)
         .then(res => {
-          dispatch({type: CHORES_ADD, payload: res.body});
+          dispatch({type: CHORE_ADD, payload: res.body});
         });
     };
   };
