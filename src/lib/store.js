088dabaddefa0f02a07787a3d24bca731098c814
michael lennerblom <mlennerblom@gmail.comgit config --listgit config --global user.name Michael Lennerblomgit config --global user.email mlennerblom@gmail.comgit config --listgit config --global user.name Michael>
2019-01-01 18:37:13 -0800

    added redux store and started reducers

---
+++ src/lib/store.js
@@ -0,0 +1,9 @@
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+import choresReducer from './choresReducer';
+import userReducer from './userReducer';
+
+const appReducer = combineReducers({chores: choresReducer, user: userReducer});
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+export default () => createStore(appReducer, composeEnhancers(applyMiddleware(thunk)));
