43934747a2d4bfe97800b94f7b33a34c2d22e379
michael lennerblom <mlennerblom@gmail.comgit config --listgit config --global user.name Michael Lennerblomgit config --global user.email mlennerblom@gmail.comgit config --listgit config --global user.name Michael>
2019-01-05 11:20:55 -0800

    completed reducers, added user form, added state, dispatch, and views in Dashboard

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -3,16 +3,65 @@ import { connect } from 'react-redux';
 import ChoreForm from './choreForm';
 import ChoreItem from './choreItem';
 
-import {choreAdd} from '../lib/choresReducer';
+import {choreAdd, choreAsyncAdd, choreFetch, choreUpdate, choreDelete} from '../lib/choresReducer';
+import {userAdd, userAsyncAdd, userFetch, userUpdate, userDelete} from '../lib/userReducer';
 class Dashboard extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            choreView: false,
+            userView: false,
+
+        }
+      }
+
+      componentDidMount() {
+        this.props.safetyFetch();
+        this.props.qualityFetch();
+        this.props.deliveryFetch();
+        this.props.costFetch();
+        this.props.moraleFetch();
+        this.props.rollupFetch();
+        }
+    
+        // componentDidUpdate() {
+        //   this.props.safetyFetch();
+        //   this.props.qualityFetch();
+        //   this.props.deliveryFetch();
+        //   this.props.costFetch();
+        //   this.props.moraleFetch();
+        //   this.props.rollupFetch(); 
+        // }
+    
+        updateView = (e) => {
+          if(e.target.name === "chore"){
+          this.setState({safetyView: true});
+          } 
+          else if(e.target.name === "user"){
+            this.setState({qualityView: true});
+          }
+      }
+    
+        returnView = () => {
+    
+          this.setState({choreView: false});
+          this.setState({userView: false});
+          }
     render() {
         return (
             <Fragment>
               <h1>add dashboard page</h1>
-              <ChoreForm onComplete={this.props.choreAdd}/>
+              {this.state.choreView && <ChoreForm onComplete={this.props.choreAdd}/>}
               <ul>
               {this.props.chore.map((chore)=> <li key={chore.id}>
               <ChoreItem chore={chore} onComplete={this.props.choreUpdate} onRemove={this.props.choreDelete} /></li>)}
+            </ul>
+
+              {this.state.userView && <userForm onComplete={this.props.userAdd}/>}
+              <ul>
+              {this.props.user.map((user)=> <li key={user.id}>
+              <userItem user={user} onComplete={this.props.userUpdate} onRemove={this.props.userDelete} /></li>)}
             </ul>
             </Fragment>
         );
@@ -21,10 +70,22 @@ class Dashboard extends Component {
 
 const mapStateToProps = (state) => ({ 
     chore: state.chore,
+    user: state.user,
 });
 
 const mapDispatchToProps = (dispatch) => ({
     choreAdd: chore => dispatch(choreAdd(chore)),
+    choreAsyncAdd: chore => dispatch(choreAsyncAdd(chore)),
+    choreFetch: chore => dispatch(choreFetch(chore)),
+    choreUpdate: chore => dispatch(choreUpdate(chore)),
+    choreDelete: chore => dispatch(choreDelete(chore)),
+
+    userAdd: user => dispatch(userAdd(user)),
+    userAsyncAdd: user => dispatch(userAsyncAdd(user)),
+    userFetch: user => dispatch(userFetch(user)),
+    userUpdate: user => dispatch(userUpdate(user)),
+    userDelete: user => dispatch(userDelete(user)),
+
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
---
+++ src/components/userForm.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import uuid from 'uuid/v4';
+
+export default class UserFprm extends Component {
+    constructor(props) {
+        super(props);
+        this.defaultState = {
+            name: '',
+            access_level: 'user',
+            chore: '',
+            email: '',
+            phone: '', //or should it be Number??
+        };
+
+        const initialState = this.props.user || this.defaultState;
+    
+        this.state = {...initialState}
+      }
+    
+      onSubmit = (e) => {
+          e.preventDefault();
+          this.props.onComplete(this.state);
+          this.setState({...this.defaultState, id: uuid()});
+          console.log(this.state);
+      };
+      onChange = (e) => {
+        const val =
+        e.target.type === "checkbox"
+          ? e.target.checked
+          : e.target.value;
+    
+      const changedBit = {
+        [e.target.name]: val
+      };
+      this.setState(changedBit);
+      }
+    render() {
+        return (
+            <fieldset>
+        <form onSubmit={this.onSubmit} onChange={this.onChange}>
+            <label>Name
+              <input name="name" value = {this.state.name}/>
+            </label>
+            <label>Access Level
+              <input name="access_level" value = {this.state.access_level}/>
+            </label>
+            <label>Chore
+              <input name="chore" value = {this.state.chore}/>
+            </label>
+            <label>Email
+              <input name="email" value = {this.state.email}/>
+            </label>
+            <label>Phone
+              <input name="phone" value = {this.state.phone}/>
+            </label>
+            <button>submit</button>
+        </form>
+        </fieldset>
+        );
+    }
+}
